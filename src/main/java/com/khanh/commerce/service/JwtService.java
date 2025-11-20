f1240eab0fb11b9b2f5d6851943a7d56cbbb80dd
Nguyen Ngoc Khanh <nguyenngockhanh231003@gmail.com>
2025-11-20 11:39:29 +0700

    code service

---
+++ src/main/java/com/khanh/commerce/service/JwtService.java
@@ -0,0 +1,93 @@
+package com.khanh.commerce.service;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+
+import java.nio.charset.StandardCharsets;
+import java.security.Key;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Function;
+
+@Service
+public class JwtService {
+    private final String SECRET_KEY;
+
+    public JwtService(@Value("${jwt.secret.key}") String secretKey) {
+        this.SECRET_KEY = secretKey;
+    }
+
+    private Key getSignInKey() {
+        byte[] keyBytes = SECRET_KEY.getBytes(StandardCharsets.UTF_8);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+
+    public String generateToken(UserDetails userDetails) {
+        return generateToken(new HashMap<>(), userDetails, 1000 * 60 * 60);
+    }
+
+    public String generateToken(
+            Map<String, Object> extraClaims,
+            UserDetails userDetails,
+            long expiration
+    ) { // payload
+        extraClaims.put("authorities", userDetails.getAuthorities());
+        return Jwts.builder()
+                // --- PAYLOAD ---
+                .setClaims(extraClaims)
+                .setSubject(userDetails.getUsername())
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + expiration))
+                // --- HEADER ---
+                .signWith(getSignInKey(), SignatureAlgorithm.HS256) // <-- {"alg": "HS256"}
+                .compact();
+    }
+
+    // REFRESH TOKEN
+    public String generateRefreshToken(UserDetails userDetails) {
+        return generateToken(new HashMap<>(), userDetails, 1000 * 60 * 60 * 24 * 7);
+    }
+
+    public String extractUsername(String token) {
+        return extractClaim(token, Claims::getSubject);
+    }
+    
+    public boolean isTokenValid(String token, UserDetails userDetails) {
+        final String username = extractUsername(token);
+        return (username.equals(userDetails.getUsername())) && !isTokenExpired(token);
+    }
+
+    private boolean isTokenExpired(String token) {
+        return extractExpiration(token).before(new Date());
+    }
+
+    private Date extractExpiration(String token) {
+        return extractClaim(token, Claims::getExpiration);
+    }
+
+    private Claims extractAllClaims(String token) {
+        // --- ĐÂY LÀ "KIỂM TRA VÉ" ---
+        // Hàm "parserBuilder()" này làm 2 việc:
+        // 1. Nó "dịch" (decode) Header và Payload.
+        // 2. Nó dùng "getSignInKey()" (chìa khóa bí mật)
+        //    để "kiểm tra chữ ký" (Signature).
+        //
+        // (Nếu chữ ký giả/vé hết hạn, nó sẽ NÉM LỖI ngay tại đây)
+        return Jwts.parserBuilder()
+                .setSigningKey(getSignInKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
+        final Claims claims = extractAllClaims(token);
+        return claimsResolver.apply(claims);
+    }
+}
