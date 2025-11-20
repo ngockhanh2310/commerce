e1b9fae655ec1ca031990f400f66e170f3843dfd
Nguyen Ngoc Khanh <nguyenngockhanh231003@gmail.com>
2025-11-20 11:31:40 +0700

    repository

---
+++ src/main/java/com/khanh/commerce/repository/OrderRepository.java
@@ -0,0 +1,12 @@
+package com.khanh.commerce.repository;
+
+import com.khanh.commerce.entity.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface OrderRepository extends JpaRepository<Order, Long> {
+    List<Order> findByUserId(Long userId);
+}
---
+++ src/main/java/com/khanh/commerce/repository/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.khanh.commerce.repository;
+
+import com.khanh.commerce.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
+}
